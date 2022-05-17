62a6e35ed31a93298122a596362a49d9cdf2689a
Maxim Mitin <maximmitin@MacBook-Air-Maxim.local>
2022-05-17 23:21:35 +0300

    first commit

---
+++ MapPointers/ViewController.swift
@@ -0,0 +1,42 @@
+//
+//  ViewController.swift
+//  MapPointers
+//
+//  Created by Maxim Mitin on 17.05.22.
+//
+
+import UIKit
+import MapKit
+import CoreLocation
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var map: MKMapView!
+    @IBOutlet weak var routeButton: UIButton!
+    @IBOutlet weak var resetButton: UIButton!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        routeButton.isHidden = true
+        resetButton.isHidden = true
+
+    }
+    
+    
+// MARK: - Buttons
+    
+    @IBAction func addAdressTaped(_ sender: UIButton) {
+        print("add tap")
+    }
+    
+    @IBAction func routeTapped(_ sender: UIButton) {
+        print("route tap")
+    }
+    
+    @IBAction func resetTapped(_ sender: UIButton) {
+        print("reset tap")
+    }
+}
+
