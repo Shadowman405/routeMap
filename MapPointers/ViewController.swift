36abb16542eebf0733d149636e2d6d04aebe1799
Maxim Mitin <maximmitin@MacBook-Air-Maxim.local>
2022-05-18 20:32:36 +0300

    add placemarks funcs

--- MapPointers/ViewController.swift
+++ MapPointers/ViewController.swift
@@ -15,6 +15,8 @@ class ViewController: UIViewController {
     @IBOutlet weak var routeButton: UIButton!
     @IBOutlet weak var resetButton: UIButton!
     
+    var annotationArray = [MKPointAnnotation]()
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -28,8 +30,8 @@ class ViewController: UIViewController {
 // MARK: - Buttons
     
     @IBAction func addAdressTaped(_ sender: UIButton) {
-        alertAddAdress(title: "Add", placeholder: "Enter adress") { text in
-            print(text)
+        alertAddAdress(title: "Add", placeholder: "Enter adress") { [self] text in
+            setupPlacemark(adressPlace: text)
         }
     }
     
@@ -39,6 +41,34 @@ class ViewController: UIViewController {
     
     @IBAction func resetTapped(_ sender: UIButton) {
         print("reset tap")
+    }
+    
+    private func setupPlacemark(adressPlace: String) {
+        let geocoder = CLGeocoder()
+        geocoder.geocodeAddressString("Минск, Притыцкого 78") { [self] placemarks, error in
+            if let error = error {
+                print(error.localizedDescription)
+                alertError(title: "Error", message: "Server unreacheable, try later")
+                return
+            }
+            
+            guard let placemarks = placemarks else { return }
+            let placemark = placemarks.first
+            
+            let annotation = MKPointAnnotation()
+            annotation.title = adressPlace
+            guard let placemarkLocation = placemark?.location else { return }
+            annotation.coordinate = placemarkLocation.coordinate
+            
+            annotationArray.append(annotation)
+            
+            if annotationArray.count > 2 {
+                routeButton.isHidden = false
+                resetButton.isHidden = false
+            }
+            
+            map.showAnnotations(annotationArray, animated: true)
+        }
     }
 }
 
